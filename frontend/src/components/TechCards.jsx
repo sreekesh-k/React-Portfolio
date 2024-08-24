7b334bc5a425789529387a768eb873126a9c2141
Sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-08-24 12:59:52 +0530

    svgs

--- frontend/src/components/TechCards.jsx
+++ frontend/src/components/TechCards.jsx
@@ -1,17 +1,52 @@
 import React from "react";
+import { FaReact, FaDocker, FaNodeJs } from "react-icons/fa";
+import { SiMongodb, SiTailwindcss } from "react-icons/si";
+
+const icons = {
+  FaReact: (
+    <FaReact
+      className="w-full h-full drop-shadow-md transition-transform duration-300 ease-in-out group-hover:scale-105"
+      color="#61DBFB"
+    />
+  ),
+  FaDocker: (
+    <FaDocker
+      className="w-full h-full drop-shadow-md transition-transform duration-300 ease-in-out group-hover:scale-105"
+      color="#2496ED"
+    />
+  ),
+  FaNodeJs: (
+    <FaNodeJs
+      className="w-full h-full drop-shadow-md transition-transform duration-300 ease-in-out group-hover:scale-105"
+      color="#68A063"
+    />
+  ),
+  SiMongodb: (
+    <SiMongodb
+      className="w-full h-full drop-shadow-md transition-transform duration-300 ease-in-out group-hover:scale-105"
+      color="#47A248"
+    />
+  ),
+  SiTailwindcss: (
+    <SiTailwindcss
+      className="w-full h-full drop-shadow-md transition-transform duration-300 ease-in-out group-hover:scale-105"
+      color="#38B2AC"
+    />
+  ),
+};
 
 const TechCard = ({ logo, altText, className, isActive, onClick }) => {
   return (
     <div
-      className={`relative flex flex-col gap-2 items-center justify-center group cursor-pointer `}
+      className={`relative flex flex-col gap-2 items-center justify-center group cursor-pointer`}
       onClick={onClick}
     >
-      <div className={`grid place-items-center ${isActive ? className : ""}`}>
-        <img
-          src={logo}
-          alt={altText}
-          className={`object-contain w-10 sm:w-20 aspect-square drop-shadow-md transition-transform duration-300 ease-in-out group-hover:scale-105`}
-        />
+      <div
+        className={`grid place-items-center ${
+          isActive ? className : ""
+        } w-10 sm:w-[4rem] aspect-square`}
+      >
+        {icons[logo] || <span>{altText}</span>}
       </div>
       <div className="relative w-full h-6">
         <div
