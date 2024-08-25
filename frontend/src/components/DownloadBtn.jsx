d6b1bb2b4a30b6651fee47dc2bf19402a3408565
Sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-08-25 17:56:59 +0530

    cache improvements

--- frontend/src/components/DownloadBtn.jsx
+++ frontend/src/components/DownloadBtn.jsx
@@ -1,17 +1,9 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import Cookies from "js-cookie";
+import React, { useState, useEffect } from "react";
 import { FaCheck } from "react-icons/fa";
 
 function DownloadBtn() {
   const [isDownloaded, setIsDownloaded] = useState(false);
-
-  useEffect(() => {
-    const downloaded = Cookies.get("resumeDownloaded");
-    if (downloaded) {
-      setIsDownloaded(true);
-    }
-  }, []);
+  const [timeoutId, setTimeoutId] = useState(null);
 
   const handleDownload = () => {
     const link = document.createElement("a");
@@ -20,32 +12,38 @@ function DownloadBtn() {
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
+
     setIsDownloaded(true);
-    Cookies.set("resumeDownloaded", "true", { expires: 1 / 24 });
+
+    const id = setTimeout(() => {
+      setIsDownloaded(false);
+    }, 5 * 1000);
+    setTimeoutId(id);
   };
+
+  useEffect(() => {
+    return () => clearTimeout(timeoutId);
+  }, [timeoutId]);
+
   return (
-    <>
-      <div className="flex gap-5 items-center justify-center">
-        <button
-          className={`mybtn grid h-10 place-items-center text-center ${
-            isDownloaded ? "opacity-50" : "hover:scale-105"
-          } px-5 select-none`}
-          onClick={handleDownload}
-          disabled={isDownloaded}
-        >
-          <span> </span>
-          <span> </span>
-          <span> </span>
-          <span> </span>
-          <span className="font-popins text-xs md:text-base uppercase font-medium select-none">
-            {isDownloaded ? "Downloaded" : "Download Resume"}
-          </span>
-        </button>
-        {isDownloaded && <FaCheck size={14} color="#4CAF50" />}
-        {/* <p className="text-[.5rem] font-roboto">20 Downloads</p> */}
-      </div>
-     
-    </>
+    <div className="flex gap-5 items-center justify-center">
+      <button
+        className={`mybtn grid h-10 place-items-center text-center ${
+          isDownloaded ? "opacity-50" : "hover:scale-105"
+        } px-5 select-none`}
+        onClick={handleDownload}
+        disabled={isDownloaded}
+      >
+        <span> </span>
+        <span> </span>
+        <span> </span>
+        <span> </span>
+        <span className="font-popins text-xs md:text-base uppercase font-medium select-none">
+          {isDownloaded ? "Downloaded" : "Download Resume"}
+        </span>
+      </button>
+      {isDownloaded && <FaCheck size={14} color="#4CAF50" />}
+    </div>
   );
 }
 
--- frontend/src/modules/Skills.jsx
+++ frontend/src/modules/Skills.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
+import Cookies from "js-cookie";
 import { FaInfoCircle } from "react-icons/fa";
+
 const gh_token = import.meta.env.REACT_APP_GH_TOKEN;
 
 function Skills({ theme }) {
@@ -7,7 +9,7 @@ function Skills({ theme }) {
   const [langColorMap, setLangColorMap] = useState({});
 
   useEffect(() => {
-    const getLanguageChart = async () => {
+    const fetchData = async () => {
       const headers = { Authorization: `Bearer ${gh_token}` };
       const response = await fetch("https://api.github.com/user/repos", {
         headers,
@@ -42,6 +44,7 @@ function Skills({ theme }) {
         for (let k in totalLangShare)
           totalLangShare[k] = ((totalLangShare[k] / total) * 100).toFixed(2);
       }
+
       const colorMap = {};
       Object.keys(totalLangShare).forEach((language) => {
         let color;
@@ -56,66 +59,79 @@ function Skills({ theme }) {
         };
       });
 
+      Cookies.set("langShare", JSON.stringify(totalLangShare), {
+        expires: 10 / (24 * 60),
+      }); 
+      Cookies.set("langColorMap", JSON.stringify(colorMap), {
+        expires: 10 / (24 * 60),
+      }); 
+
       setLangShare(totalLangShare);
       setLangColorMap(colorMap);
     };
 
-    getLanguageChart();
+    const cachedLangShare = Cookies.get("langShare");
+    const cachedLangColorMap = Cookies.get("langColorMap");
+
+    if (cachedLangShare && cachedLangColorMap) {
+      setLangShare(JSON.parse(cachedLangShare));
+      setLangColorMap(JSON.parse(cachedLangColorMap));
+    } else {
+      fetchData();
+    }
   }, [gh_token]);
 
   return (
-    <>
-      <section className="p-5 md:p-10">
-        <div className="container p-5 mx-auto flex flex-col items-center">
-          <h1 className="text-4xl md:text-5xl font-popins">
-            <a
-              href="https://github.com/sreekesh-k"
-              target="_blank"
-              rel="noopener noreferrer"
+    <section className="p-5 md:p-10">
+      <div className="container p-5 mx-auto flex flex-col items-center">
+        <h1 className="text-4xl md:text-5xl font-popins">
+          <a
+            href="https://github.com/sreekesh-k"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GITHUB{" "}
+          </a>
+          <span className="text-violet-500">STATS</span>
+        </h1>
+        <p className="gap-2 my-6 text-xs opacity-70 capitalize font-semibold text-justify px-5">
+          <FaInfoCircle className="inline mb-[.2rem] mr-2" />
+          This shows most used languages On my personal{" "}
+          <a
+            href="https://github.com/sreekesh-k"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-violet-500 uppercase"
+          >
+            github
+          </a>{" "}
+          profile.
+        </p>
+        <div className="flex flex-row gap-[.3rem] md:gap-4 h-56 md:h-96">
+          {Object.keys(langShare).map((language, index) => (
+            <div
+              key={index}
+              className="flex flex-col items-center h-full justify-end"
             >
-              GITHUB{" "}
-            </a>
-            <span className="text-violet-500">STATS</span>
-          </h1>
-          <p className="gap-2 my-6 text-xs opacity-70 capitalize font-semibold text-justify px-5">
-            <FaInfoCircle className="inline mb-[.2rem] mr-2" />
-            This shows most used languages On my personal{" "}
-            <a
-              href="https://github.com/sreekesh-k"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-violet-500 uppercase"
-            >
-              github
-            </a>{" "}
-            profile.
-          </p>
-          <div className="flex flex-row gap-[.3rem] md:gap-4 h-56 md:h-96">
-            {Object.keys(langShare).map((language, index) => (
               <div
-                key={index}
-                className="flex flex-col items-center h-full justify-end"
-              >
-                <div
-                  className="w-6 md:w-12 xl:w-16 rounded-t-sm md:rounded-t-md shadow-sm"
-                  style={{
-                    height: `${langShare[language]}%`,
-                    maxHeight: "100%",
-                    backgroundColor: langColorMap[language].color,
-                  }}
-                ></div>
-                <span className="mt-2 text-[.5rem] md:text-[.6rem] xl:text-xs font-semibold">
-                  {language}
-                </span>
-                <span className="text-[.4rem] md:text-[.5rem] xl:text-xs font-semibold">
-                  {langColorMap[language].percentage}%
-                </span>
-              </div>
-            ))}
-          </div>
+                className="w-6 md:w-12 xl:w-16 rounded-t-sm md:rounded-t-md shadow-sm"
+                style={{
+                  height: `${langShare[language]}%`,
+                  maxHeight: "100%",
+                  backgroundColor: langColorMap[language]?.color || "#000000",
+                }}
+              ></div>
+              <span className="mt-2 text-[.5rem] md:text-[.6rem] xl:text-xs font-semibold">
+                {language}
+              </span>
+              <span className="text-[.4rem] md:text-[.5rem] xl:text-xs font-semibold">
+                {langColorMap[language]?.percentage || "0.00"}%
+              </span>
+            </div>
+          ))}
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 }
 
