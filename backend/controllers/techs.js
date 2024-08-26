9e8d58700968accaccd569b313630806dd2b6190
Sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-08-26 16:42:04 +0530

    backend changes

--- backend/controllers/techs.js
+++ backend/controllers/techs.js
@@ -1,14 +1,26 @@
-const TechCard = require('../models/TechCard'); 
+const TechCard = require("../models/TechCard");
 
 const getTechs = async (req, res) => {
   try {
-    const techCards = await TechCard.find();
+    const techCards = await TechCard.find({}, { description: 0 });
     res.json(techCards);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 };
-
+const getTechById = async (req, res) => {
+  try {
+    const techCard = await TechCard.findById(req.params.id);
+    if (techCard) {
+      res.json({ description: techCard.description });
+    } else {
+      res.status(404).json({ message: "TechCard not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 module.exports = {
   getTechs,
+  getTechById,
 };
--- backend/routes/techRoutes.js
+++ backend/routes/techRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const techs = require("../controllers/techs")
 
 router.get('/', techs.getTechs);
-
+router.get('/:id', techs.getTechById);
 
 module.exports = router;
