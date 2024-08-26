01438259ec514cf6d9e25ea7e628cd5edccf7a5e
Sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-08-26 16:34:38 +0530

    dynamic test

--- frontend/src/modules/Skills.jsx
+++ frontend/src/modules/Skills.jsx
@@ -1,85 +1,95 @@
 import React, { useEffect, useState } from "react";
 import Cookies from "js-cookie";
 import { FaInfoCircle } from "react-icons/fa";
-
-const gh_token = import.meta.env.REACT_APP_GH_TOKEN;
-
 function Skills({ theme }) {
   const [langShare, setLangShare] = useState({});
   const [langColorMap, setLangColorMap] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
-      const headers = { Authorization: `Bearer ${gh_token}` };
-      const response = await fetch("https://api.github.com/user/repos", {
-        headers,
-      });
-      const data = await response.json();
-
-      const repoMap = data.map((val) => ({
-        name: val.name,
-        langURL: val.languages_url,
-      }));
-      const repoLangMapPromise = repoMap.map((repo) =>
-        fetch(repo.langURL, { headers })
-      );
-      const responses = await Promise.all(repoLangMapPromise);
-      const repoAndLanguages = await Promise.all(
-        responses.map((res) => res.json())
-      );
-
-      let totalLangShare = [];
-      if (repoAndLanguages) {
-        totalLangShare = repoAndLanguages.reduce((acc, val) => {
-          for (let key in val) {
-            const exclusion = ["CSS", "SCSS", "Hack", "Shell"];
-            if (exclusion.indexOf(key) !== -1) continue;
-            if (acc[key]) acc[key] += val[key];
-            else acc[key] = val[key];
-          }
-          return acc;
-        }, {});
-        let total = 0;
-        for (let k in totalLangShare) total += totalLangShare[k];
-        for (let k in totalLangShare)
-          totalLangShare[k] = ((totalLangShare[k] / total) * 100).toFixed(2);
+      try {
+        const response = await fetch(
+          `${import.meta.env.REACT_APP_PUBLIC_API}/github/languages`
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+          fetchData();
+        }
+        const data = await response.json();
+        setLangShare(data.totalLangShare);
+        setLangColorMap(data.colorMap);
+        Cookies.set("totalLangShare", JSON.stringify(data.totalLangShare), {
+          expires: 10 / (24 * 60),
+        });
+        Cookies.set("colorMap", JSON.stringify(data.colorMap), {
+          expires: 10 / (24 * 60),
+        });
+        setLoading(false);
+      } catch (error) {
+        setError(error.message);
+        setLoading(false);
       }
-
-      const colorMap = {};
-      Object.keys(totalLangShare).forEach((language) => {
-        let color;
-        do {
-          color =
-            "#" +
-            ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, "0");
-        } while (colorMap[color] !== undefined);
-        colorMap[language] = {
-          color,
-          percentage: totalLangShare[language],
-        };
-      });
-
-      Cookies.set("langShare", JSON.stringify(totalLangShare), {
-        expires: 10 / (24 * 60),
-      }); 
-      Cookies.set("langColorMap", JSON.stringify(colorMap), {
-        expires: 10 / (24 * 60),
-      }); 
-
-      setLangShare(totalLangShare);
-      setLangColorMap(colorMap);
     };
 
-    const cachedLangShare = Cookies.get("langShare");
-    const cachedLangColorMap = Cookies.get("langColorMap");
+    const cachedLangShare = Cookies.get("totalLangShare");
+    const cachedLangColorMap = Cookies.get("colorMap");
 
     if (cachedLangShare && cachedLangColorMap) {
       setLangShare(JSON.parse(cachedLangShare));
       setLangColorMap(JSON.parse(cachedLangColorMap));
+      setLoading(false);
     } else {
       fetchData();
     }
-  }, [gh_token]);
+  }, []);
+
+  if (loading) {
+    return (
+      <section className="p-5 md:p-10">
+        <div className="container p-5 mx-auto flex flex-col items-center">
+          <h1 className="text-4xl md:text-5xl font-popins">
+            <a
+              href="https://github.com/sreekesh-k"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GITHUB{" "}
+            </a>
+            <span className="text-violet-500">STATS</span>
+          </h1>
+          <p className="gap-2 my-6 text-xs opacity-70 capitalize font-semibold text-justify px-5 animate-pulse ">
+            <FaInfoCircle className="inline mb-[.2rem] mr-2 animate-ping" />
+            Loading your GitHub stats...
+          </p>
+        </div>
+      </section>
+    );
+  }
+
+  if (error) {
+    return (
+      <section className="p-5 md:p-10">
+        <div className="container p-5 mx-auto flex flex-col items-center">
+          <h1 className="text-4xl md:text-5xl font-popins">
+            <a
+              href="https://github.com/sreekesh-k"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GITHUB{" "}
+            </a>
+            <span className="text-violet-500">STATS</span>
+          </h1>
+          <p className="gap-2 my-6 text-xs opacity-70 capitalize font-semibold text-justify px-5 ">
+            <FaInfoCircle className="inline mb-[.2rem] mr-2 " />
+            An error occurred: {error} , please try refreshing the page!
+          </p>
+        </div>
+      </section>
+    );
+  }
 
   return (
     <section className="p-5 md:p-10">
@@ -96,14 +106,14 @@ function Skills({ theme }) {
         </h1>
         <p className="gap-2 my-6 text-xs opacity-70 capitalize font-semibold text-justify px-5">
           <FaInfoCircle className="inline mb-[.2rem] mr-2" />
-          This shows most used languages On my personal{" "}
+          This shows most used languages on my personal{" "}
           <a
             href="https://github.com/sreekesh-k"
             target="_blank"
             rel="noopener noreferrer"
             className="text-violet-500 uppercase"
           >
-            github
+            GitHub
           </a>{" "}
           profile.
         </p>
