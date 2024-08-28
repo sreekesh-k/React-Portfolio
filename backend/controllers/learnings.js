11b83be8d1c6c0d7813af6dcd7158e94e20a5fb7
Sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-08-28 12:40:10 +0530

    logo

--- backend/controllers/learnings.js
+++ backend/controllers/learnings.js
@@ -19,6 +19,7 @@ const getMarkdownFile = async (req, res, fileName) => {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>${fileName}</title>
+        <link rel="icon" type="image/png" href="https://sreekeshkprabhu.me/logos/sreekeshkprabhu.png" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.1.0/github-markdown.min.css">
         <style>
           body {
