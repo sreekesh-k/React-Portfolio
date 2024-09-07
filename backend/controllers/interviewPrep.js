90af3524952d544e4485dbb9e3f827e6fc223435
Sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-09-07 07:57:18 +0530

    small change

--- backend/controllers/interviewPrep.js
+++ backend/controllers/interviewPrep.js
@@ -18,7 +18,7 @@ const getMarkdownFile = async (req, res, fileName) => {
       <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Sreekesh K Prabhu - Interview Prep Java</title>
+        <title>Sreekesh K Prabhu - Interview Prep</title>
         <link rel="icon" type="image/png" href="https://sreekeshkprabhu.me/logos/sreekeshkprabhu.png" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/5.1.0/github-markdown.min.css">
         <style>
